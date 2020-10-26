fda4a0582d9379cad225895674def2754d287d0f
Suraj Bahadur <surajbahadur58@gmail.com>
2020-10-26 22:25:17 +0530

    menu items updated with react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 
+import {Switch, Route} from 'react-router-dom'
+
+const HatsPage = () => {
+  return (<>
+    <h1> Hats Page</h1>
+    </>
+  )
+}
+
 function App() {
   return (
-    <div > 
-
-      <HomePage/>
-      
+    <div >
+      <Switch>
+      <Route exact path='/' component={HomePage}></Route> 
+      <Route path='/hats' component={HatsPage}></Route> 
+      </Switch>   
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -33,7 +33,7 @@ class Directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
                   size: 'large',
                   id: 4,
-                  size : 'large',
+                  
                   linkUrl: 'shop/womens'
                 },
                 {
@@ -41,7 +41,7 @@ class Directory extends React.Component {
                   imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
                   size: 'large',
                   id: 5,
-                  size : 'large',
+                  
                   linkUrl: 'shop/mens'
                 }
             ]
@@ -53,8 +53,8 @@ class Directory extends React.Component {
     render(){
         return (
             <div className="directory-menu">
-                {this.state.sections.map(( {title, imageUrl, id, size}) => (
-                        <MenuItem key={id}  title={title} imageUrl={imageUrl} size = {size}/>
+                {this.state.sections.map(( {title, imageUrl, id, size , linkUrl }) => (
+                        <MenuItem key={id}  title={title} imageUrl={imageUrl} size = {size} linkUrl ={linkUrl}/>
                 )
                 
                 )}
