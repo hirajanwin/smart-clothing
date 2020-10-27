cdc7c293638ce469b77311575dd7c174a3dff8b7
Suraj Bahadur <surajbahadur58@gmail.com>
2020-10-27 21:07:35 +0530

    adding header logo, shop link and contact link

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import {Switch, Route} from 'react-router-dom'
 
 import ShopPage from './pages/shop/shop.component'
 
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     <div >
+      <Header/>
       <Switch>
       <Route exact path='/' component={HomePage}></Route> 
       <Route path='/shop' component={ShopPage}></Route> 
