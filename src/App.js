f6cf035a7e02ecf3feb0be43a7db223dadc6f81c
Suraj Bahadur <surajbahadur58@gmail.com>
2020-10-28 17:08:54 +0530

    added form layout and css

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import ShopPage from './pages/shop/shop.component'
 
 import Header from './components/header/header.component'
 
+import SignInSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Switch>
       <Route exact path='/' component={HomePage}></Route> 
       <Route path='/shop' component={ShopPage}></Route> 
+      <Route path='/signin' component={SignInSignUp}></Route>
       </Switch>   
     </div>
   );
